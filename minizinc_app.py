7e6d9ffbed03b0c467183bcde4d82529cd42e91a
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2022-02-16 02:20:57 -0500

    canvas added

--- minizinc_app.py
+++ minizinc_app.py
@@ -9,5 +9,8 @@ result = instance.solve()
 print(result)
 
 class App():
-    def __init__(self, window):
-        pass
+    def __init__(self, master):
+        canvas = Canvas(master, width=100, height=100, bg='white')
+        canvas.pack(side="bottom", fill='x')
+        canvas.create_line(10, 0, 10, 100)
+        canvas.create_line(20, 0, 20, 100)
